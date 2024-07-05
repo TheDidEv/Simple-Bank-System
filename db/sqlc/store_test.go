99c880a0234ed4843a17d999fa8248091c6ebe95
TheDidEv <thedideva@gmail.com>
2024-07-05 23:00:53 +0300

    update store

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -114,3 +114,55 @@ func TestTransferTx(t *testing.T) {
 
 	}
 }
+
+func TestTransferTxDeadLock(t *testing.T) {
+	// store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">> before", account1.Balance, account2.Balance)
+
+	// run a concurrent transfer transactions
+	n := 10
+	amount := int64(10)
+
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountId := account1.ID
+		toAccountId := account2.ID
+
+		if i%2 == 1 {
+			fromAccountId = account2.ID
+			toAccountId = account1.ID
+		}
+
+		go func() {
+			ctx := context.Background()
+			_, err := testStore.TransferTx(ctx, TransferTxParams{
+				FromAccountID: fromAccountId,
+				ToAccountID:   toAccountId,
+				Amount:        amount,
+			})
+
+			errs <- err
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		// check the final update balance
+		updateAccount1, err := testStore.GetAccount(context.Background(), account1.ID)
+		require.NoError(t, err)
+
+		updateAccount2, err := testStore.GetAccount(context.Background(), account2.ID)
+		require.NoError(t, err)
+
+		fmt.Println(">> after", updateAccount1.Balance, updateAccount2.Balance)
+		require.Equal(t, account1.Balance, updateAccount1.Balance)
+		require.Equal(t, account2.Balance, updateAccount2.Balance)
+
+	}
+}
